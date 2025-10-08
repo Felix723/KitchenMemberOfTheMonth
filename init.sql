6438917dbff61e187b76fed1cb2f06ada96df377
Felix723 <s224313@dtu.dk>
2025-10-08 19:56:11 +0200

    remove duckdb from docker and clear the coffee.db

--- init.sql
+++ init.sql
@@ -1,10 +1,14 @@
+drop table if exists user;
+drop table if exists product;
+drop table if exists purchase;
+
 create table user(
 	username string primary key, 
 	email string, 
 	password string
 );
 insert into user values
- ('Felix', 'felix@itu.dk', 'test');
+ ('felix', 'felix@itu.dk', '1');
 
 create table product(
 	task_category string primary key,
@@ -24,3 +28,6 @@ create table purchase(
 	username string,
 	primary key (awardTime, points, username)
 );
+
+insert into purchase values
+ ('2025-10-11 9:55'::timestamp, 50, 'felix'),
